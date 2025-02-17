86765e863a86067d96fcd0a32363e3556369b757
Alira13 <Bezglasnaya@gmail.com>
2025-02-17 13:41:37 +0300

    add initial for design patterns and multithreading

---
+++ MultiThreading/app/src/main/java/com/example/multithreading/ThreadRunner.kt
@@ -0,0 +1,33 @@
+package com.example.multithreading
+
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
+
+class ThreadRunner {
+    fun runThreads(): Map<String, String> {
+        val threadInfo = mutableMapOf<String, String>()
+
+        // Добавьте в Map имя главного потока и описание его работы
+        threadInfo[Thread.currentThread().name] = "Главный поток, который управляет выполнением"
+        // Запустите три потока, добавляя в Map имя потока и описание его работы
+        val thread1 = thread {
+            sleep(2000)
+            threadInfo[Thread.currentThread().name] = "Выполняет вычисления 1"
+        }
+        val thread2 = thread {
+            threadInfo[Thread.currentThread().name] = "Выполняет вычисления 2"
+        }
+        val thread3 = thread {
+            threadInfo[Thread.currentThread().name] = "Выполняет вычисления 3"
+        }
+        // Дождитесь завершения потоков, чтобы они успели записать свои имена в Map
+        thread1.join()
+        thread2.join()
+        thread3.join()
+        return threadInfo
+    }
+}
+
+fun main() {
+    println(ThreadRunner().runThreads())
+}
