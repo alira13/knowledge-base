c294801dc7c229431ed7dbe1e4982fb4907a5201
Alira13 <Bezglasnaya@gmail.com>
2025-02-17 13:42:36 +0300

    Correct simpliest example

--- MultiThreading/app/src/main/java/com/example/multithreading/SimpliestExample.kt
+++ MultiThreading/app/src/main/java/com/example/multithreading/SimpliestExample.kt
@@ -4,47 +4,20 @@ import java.lang.Thread.sleep
 import kotlin.concurrent.thread
 import kotlin.random.Random.Default.nextInt
 
-/*fun main() {
-    /*  thread {
-          repeat(1000) {
-              println("*")
-              sleep(1000)
-          }
-      }
-      repeat(1000)
-      {
-          println("-")
-          sleep(2000)
-      }
-
-     */
-    runGame()
-}
-
- */
-
 fun main() {
-    println("Enter num")
-    val userNum = readln().trim().toInt()
-    var win = false
-
-    //timer
     thread {
-        var i = 1
-        while (!win) {
-            println(i++)
+        repeat(1000) {
+            println("*")
             sleep(1000)
         }
     }
-
-    thread {
-        while (true) {
-            val compNum = nextInt(0, 1_000_000_001)
-            if (userNum == compNum) {
-                println("I win. Your number is $compNum")
-                win = true
-                break
-            }
-        }
+    repeat(1000)
+    {
+        println("-")
+        sleep(2000)
     }
+
+
 }
+
+
