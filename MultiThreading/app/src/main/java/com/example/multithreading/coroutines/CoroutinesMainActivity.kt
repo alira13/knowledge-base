58ea2b806a045aba25482c083ca587351c468bfb
Alira13 <Bezglasnaya@gmail.com>
2025-02-28 14:13:38 +0300

    Correct scope

--- MultiThreading/app/src/main/java/com/example/multithreading/coroutines/CoroutinesMainActivity.kt
+++ MultiThreading/app/src/main/java/com/example/multithreading/coroutines/CoroutinesMainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.multithreading.coroutines
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -10,7 +11,9 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.multithreading.R
 import com.example.multithreading.entities.Author
 import com.example.multithreading.entities.Book
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.concurrent.thread
@@ -20,6 +23,8 @@ class CoroutinesMainActivity : AppCompatActivity() {
     private lateinit var loadBtn: Button
     private lateinit var contentTv: TextView
     private lateinit var timerTv: TextView
+
+    private val myScope = CoroutineScope(CoroutineName("My coroutine"))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,13 +47,16 @@ class CoroutinesMainActivity : AppCompatActivity() {
             // создаем корутину и запускаем ее
             // все длительные операции у нас в suspend-функциях, которые могут приостанавливаться
             // и затем возобновляться чтобы не блокировать поток
-            GlobalScope.launch {
+            // запускаем корутину внутри myScope чтобы можно было его потом отменить=отменить все корутины
+            myScope.launch {
                 val book = loadBook()
+                Log.d("MY", "Get book $book")
                 contentTv.append("Loading book...\n\n")
                 contentTv.append(book.toString() + "\n\n")
 
                 contentTv.append("Loading author...\n\n")
                 val author = loadAuthor()
+                Log.d("MY", "Get author $author")
                 contentTv.append(author.toString() + "\n\n")
                 contentTv.append("Finish loading...\n\n")
                 // Нам нужно запустить в том же потоке, что и изменил первоначальное значение
@@ -90,6 +98,12 @@ class CoroutinesMainActivity : AppCompatActivity() {
     private suspend fun loadAuthor(): Author {
         delay(3000)
         return (Author("Oruel", "British writer"))
+    }
 
+    // отмена scope
+    // отменяет все корутины внутри scope
+    override fun onDestroy() {
+        super.onDestroy()
+        myScope.cancel()
     }
 }
