e19b19e4626783d166e8827893f334c3bb297c82
Alira13 <Bezglasnaya@gmail.com>
2025-03-11 11:41:04 +0300

    Add flow example

--- FlowAndChannels/app/src/main/java/com/example/flowandchannels/flowOnPractice/MainActivity.kt
+++ FlowAndChannels/app/src/main/java/com/example/flowandchannels/flowOnPractice/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.flowandchannels.flowOnPractice
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -15,7 +14,12 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -28,6 +32,9 @@ class MainActivity : AppCompatActivity() {
     private val repository = Repository
 
     private var job: Job? = null
+
+    // запросы пользователя
+    private var requests = Channel<String> { }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,19 +57,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadDefinitions() {
-        job?.cancel()
-        job = scope.launch {
-            searchButton.isEnabled = false
-            searchResult.text = "Loading..."
-            delay(500)
-            val word = searchRequest.text.trim().toString()
-            Log.d("MY", "$word")
-            val result = repository.loadDefinition(word).joinToString("\n\n")
-            Log.d("MY", "$result")
-
-            searchResult.text = result.ifEmpty { "Not found" }
-            searchButton.isEnabled = true
+        scope.launch {
+            requests.send(searchRequest.text.trim().toString())
         }
+    }
+
+    init {
+        requests.consumeAsFlow()
+            // делаем какое-то действие для каждого элемента flow
+            .onEach {
+                searchButton.isEnabled = false
+                searchResult.text = "Loading..."
+                searchRequest.text.trim().toString()
+            }
+            // ожидание 500, если поступает новый в течении 500
+            // то старый отменяется и новый побежить по цепочке
+            .debounce(500)
+            // преобразует один тип в другой на основе лямбды
+            .map {
+                repository.loadDefinition(it)
+            }
+            // преобразует один тип в другой на основе лямбды
+            .map {
+                it.joinToString("\n\n").ifEmpty { "Not found" }
+            }.onEach {
+                searchResult.text = it
+                searchButton.isEnabled = true
+            }
+            // создает корутину на scope и вызывает collect на flow
+            .launchIn(scope)
     }
 
     private fun initViews() {
